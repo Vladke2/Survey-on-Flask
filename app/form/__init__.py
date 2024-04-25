a67e2b85cf50abacab8fc7c48ababceee0eb0f1a
vladke <130487670+Vladke2@users.noreply.github.com>
2024-04-25 20:40:30 +0300

    Create __init__.py

---
+++ app/form/__init__.py
@@ -0,0 +1,5 @@
+from .login_form import LoginForm
+from .registration_form import RegisterForm
+from .edit_profile_form import EditProfileForm
+from .create_survey_form import CreateSurveyForm
+from .fill_survey_form import FillSurveyForm
