21d72b39a8728ee43707e5e031062250a3036ef5
vladke <130487670+Vladke2@users.noreply.github.com>
2024-04-25 20:43:25 +0300

    Create login_form.py

---
+++ app/form/login_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
